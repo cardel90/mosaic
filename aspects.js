e1bdfe693b5c909ee833ee7dc8c67d161dd7e504
cardel <cardel@o2.pl>
2015-07-23 09:21:45 +0200

    Organize aspects

--- aspects.js
+++ aspects.js
@@ -72,152 +72,109 @@ function loadAspect(cell, aspectType, aspectArguments) {
 	return result;
 }
 
-function RunningAway() {
+// ****************************************************************
+// core aspects
+
+function Looking() {
+	this.seen = [];
 }
 
-RunningAway.defaults = {
-	speedThreshold: 1,
-	angleThreshold: 0.5
+Looking.defaults = {
+	angle: Math.PI*2,
+	range: 200
 };
 
-RunningAway.prototype.prepare = function() {
-	this.hunter = undefined;
-	var tcell = this.cell;
-	var tab = _.filter(this.cell.getAspect(Looking).seen, function(c){
-		return c.hasAspect(Hunting)
-			&& c.velocity.length() > this.speedThreshold
-			&& c.velocity.dotReduced(tcell.position.minus(c.position)) > this.angleThreshold;
-	}, this);
-	if(tab.length>0) {
-		this.hunter = tab[0];
-	}
+function canvas_arrow(context, fromx, fromy, tox, toy){
+    var headlen = 10;   // length of head in pixels
+    var angle = Math.atan2(toy-fromy,tox-fromx);
+    context.moveTo(fromx, fromy);
+    context.lineTo(tox, toy);
+    context.lineTo(tox-headlen*Math.cos(angle-Math.PI/6),toy-headlen*Math.sin(angle-Math.PI/6));
+    context.moveTo(tox, toy);
+    context.lineTo(tox-headlen*Math.cos(angle+Math.PI/6),toy-headlen*Math.sin(angle+Math.PI/6));
 }
 
-RunningAway.prototype.perform = function() {
-	if(this.hunter === undefined)
-		return;
-	var f = this.cell.position.minus(this.hunter.position).normalize().scale(1.5);
-	this.cell.getAspect(Walking).applyForce(f);
-};
-
-RunningAway.prototype.draw = function(ctx) {
-	if(this.hunter) {
-		ctx.beginPath();
-		ctx.strokeStyle = 'yellow';
-		ctx.moveTo(this.cell.position.x, this.cell.position.y);
-		ctx.lineTo(this.hunter.position.x, this.hunter.position.y);
+Looking.prototype.draw = function(ctx) {
+	return;
+	ctx.beginPath();
+	ctx.strokeStyle = 'black';
+	/*
+	var m1 = [[Math.cos(this.angle),-Math.sin(this.angle)], [Math.sin(this.angle),Math.cos(this.angle)]];
+	var m2 = [[Math.cos(-this.angle),-Math.sin(-this.angle)], [Math.sin(-this.angle),Math.cos(-this.angle)]];
+	var v = this.cell.velocity.normalize().scale(this.range);
+	var v1 = v.mulMatrix(m1);
+	var p1 = this.cell.position.plus(v1);
+	var v2 = v.mulMatrix(m2);
+	var p2 = this.cell.position.plus(v2);
+	ctx.moveTo(this.cell.position.x, this.cell.position.y);
+	ctx.lineTo(p1.x, p1.y);
+	ctx.moveTo(this.cell.position.x, this.cell.position.y);
+	ctx.lineTo(p2.x, p2.y);
+	var a = v.angle();//Math.PI/4;
+	ctx.arc(this.cell.position.x, this.cell.position.y, this.range, a-this.angle, a+this.angle);
+	ctx.stroke();
+	*/
+	
+	for(var i=0; i<this.seen.length; i++) {
+		canvas_arrow(ctx, 
+			this.cell.position.x, this.cell.position.y, this.seen[i].position.x, this.seen[i].position.y
+		);
 		ctx.stroke();
 	}
+}
+
+Looking.prototype.perform = function() {
+	this.seen = this.cell.nearestCells(this.range);
+}
+
+Looking.prototype.report = function() {
+	return [this.seen.length];
+}
+
+function Walking(args) {
+	this.velocity = new Vector(0, 0);
+	this.force = new Vector(0, 0);
+	this.forceCount = 0;
+}
+
+Walking.defaults = {
+	topSpeed: 2,
+	agility: 2
 };
 
-RunningAway.prototype.priority = function() {
-	return this.hunter === undefined ? 0 : 100;
+Walking.prototype.applyForce = function(f) {
+	this.force = this.force.plus(f);
+	this.forceCount++;
 }
 
-RunningAway.prototype.report = function() {
-	return this.hunter === undefined ? [] : [1];
+Walking.prototype.prepare = function() {
+	this.force = new Vector(0, 0);
+	this.forceCount = 0;
 }
 
-function Hunting() {
-}
+Walking.prototype.perform = function() {
 
-Hunting.prototype.findPrey = function() {
-	var tcell = this.cell;
-	var tab = _.filter(this.cell.getAspect(Looking).seen, function(c){
-		return c.color != tcell.color;
-	});
-	if(tab.length > 0) {
-		var prey = _.min(tab, function(c){
-			return c.position.distance(tcell.position);
-		});
-		return prey;
+	if(this.forceCount > 0) {
+		this.force = this.force.scale(1/this.forceCount).scale(0.6*(20-this.cell.getSize())/20 + 0.7);
+		this.velocity = this.velocity.plus(this.force.scale(this.agility)).capLength(this.topSpeed);
 	}
+	
+	// for legacy
+	this.cell.velocity = this.velocity = this.velocity.scale(0.9);
+	this.cell.position = this.cell.position.plus(this.velocity);
 }
 
-Hunting.prototype.prepare = function() {
-	this.prey = this.findPrey();
+Walking.prototype.draw = function(ctx) {
+	ctx.beginPath();
+	ctx.strokeStyle = 'lime';
+	ctx.moveTo(this.cell.position.x, this.cell.position.y);
+	var t = this.cell.position.plus(this.force.scale(50));
+	ctx.lineTo(t.x, t.y);
+	ctx.stroke();
 }
 
-Hunting.prototype.perform = function() {
-	if(this.prey === undefined)
-		return;
-	var d = this.prey.position.distance(this.cell.position) - this.prey.getSize() - this.cell.getSize();
-	if(d < 5) {
-		this.cell.getAspect(Eating).feed(this.prey.getAspect(Eating).fat);
-		// should this be in eating?
-		this.prey.getAspect(Eating).fat = 0;
-		return;
-	}
-		
-	var f = this.prey.position.minus(this.cell.position).normalize().scale(1.5);
-	this.cell.getAspect(Walking).applyForce(f);
-};
-
-Hunting.prototype.draw = function(ctx) {
-	if(this.prey) {
-		ctx.beginPath();
-		ctx.strokeStyle = '#FF0000';
-		ctx.moveTo(this.cell.position.x, this.cell.position.y);
-		ctx.lineTo(this.prey.position.x, this.prey.position.y);
-		ctx.stroke();
-	}
-};
-
-Hunting.prototype.priority = function() {
-	return this.prey === undefined ? 0 : this.cell.getAspect(Eating).hunger - this.prey.position.distance(this.cell.position)/100;
-}
-
-Hunting.prototype.report = function() {
-	return [this.priority()];
-}
-
-function Mating() {
-	this.horny = 0;
-}
-
-Mating.prototype.findMate = function() {
-	var tcell = this.cell;
-	var tab = _.filter(this.cell.getAspect(Looking).seen, function(c) {
-		return c.species === tcell.species && c.gender !== tcell.gender;
-	});
-	if(tab.length > 0)
-		return tab[0];
-}
-
-Mating.prototype.prepare = function() {
-	this.horny += Math.random()*0.01;
-	this.mate = this.findMate();
-}
-
-Mating.prototype.perform = function() {
-	if(this.mate === undefined)
-		return;
-	if(this.mate.position.distance(this.cell.position) < this.cell.getSize()+this.mate.getSize()+5) {
-		
-		this.cell.makeChild();
-		this.horny = -1;
-	}
-		
-	var f = this.mate.position.minus(this.cell.position).normalize();
-	this.cell.getAspect(Walking).applyForce(f);
-};
-
-Mating.prototype.draw = function(ctx) {
-	if(this.mate) {
-		ctx.beginPath();
-		ctx.strokeStyle = '#FF77DD';
-		ctx.moveTo(this.cell.position.x, this.cell.position.y);
-		ctx.lineTo(this.mate.position.x, this.mate.position.y);
-		ctx.stroke();
-	}
-};
-
-Mating.prototype.priority = function() {
-	return this.mate === undefined ? 0 : this.horny;
-}
-
-Mating.prototype.report = function() {
-	return [this.cell.gender, this.horny];
+Walking.prototype.report = function() {
+	return [this.forceCount];
 }
 
 function FromWalls() {
@@ -285,40 +242,47 @@ FromOthers.prototype.draw = function(ctx) {
 	}
 }
 
-function Herding() {
-	this.vector = new Vector(0, 0);
+// ****************************************************************
+// eating aspects
+
+function Eating() {
+	this.fat = 10;
+	this.hunger = 0;
 }
 
-Herding.defaults = {
-	strength: 1
-};
-
-Herding.prototype.prepare = function() {
-	var vel = new Vector(0, 0);
-	var loc = new Vector(0, 0);
-	this.vector = new Vector(0, 0);
+Eating.prototype.perform = function() {	
+	this.fat -= (Math.random()*0.002)*this.fat;
 	
-	var tcell = this.cell;
-	var tab = _.filter(this.cell.getAspect(Looking).seen, function(c){
-		return c!==tcell && c.color===tcell.color && c.position.distance(tcell.position)<100;
-	});
-	var n = tab.length;
-	if(n>0) {
-		for(var i=0; i<n; i++) {
-			var c = tab[i];
-			vel = vel.plus(c.velocity);
-			loc = loc.plus(c.position);
-		}
-		var toLoc = loc.scale(1/n).minus(tcell.position).normalize();
-		var toVel = vel.scale(1/n).minus(tcell.getAspect(Walking).velocity).normalize();
-		
-		this.vector = toVel.plus(toLoc);
-	}
+	if(this.fat < 10)
+		this.hunger += (10-this.fat)/10;
+	if(this.fat > 15)
+		this.hunger = 0;
+}
+
+Eating.prototype.feed = function(amount) {
+	this.fat += amount;
+}
+
+Eating.prototype.report = function() {
+	return [this.fat, this.hunger];
+}
+
+function Photosynthesis() {
+}
+
+Photosynthesis.defaults = {
+	range: 200
 };
 
-Herding.prototype.perform = function() {
-	this.cell.getAspect(Walking).applyForce(this.vector.normalize().scale(this.strength));
-};
+Photosynthesis.prototype.perform = function() {
+	var eating = this.cell.getAspect(Eating);
+	var center = new Vector(width/2, height/2);
+	var d = this.range - (this.cell.position.distance(center));
+	if(d<0)
+		d = 0;
+	eating.feed(0.05*d/this.range);
+	this.cell.getAspect(Walking).applyForce(center.minus(this.cell.position).normalize().scale(0.1));
+}
 
 function Grazing() {
 }
@@ -383,6 +347,160 @@ Grazing.prototype.report = function() {
 	return [this.priority()];
 }
 
+function Hunting() {
+}
+
+Hunting.prototype.findPrey = function() {
+	var tcell = this.cell;
+	var tab = _.filter(this.cell.getAspect(Looking).seen, function(c){
+		return c.color != tcell.color;
+	});
+	if(tab.length > 0) {
+		var prey = _.min(tab, function(c){
+			return c.position.distance(tcell.position);
+		});
+		return prey;
+	}
+}
+
+Hunting.prototype.prepare = function() {
+	this.prey = this.findPrey();
+}
+
+Hunting.prototype.perform = function() {
+	if(this.prey === undefined)
+		return;
+	var d = this.prey.position.distance(this.cell.position) - this.prey.getSize() - this.cell.getSize();
+	if(d < 5) {
+		this.cell.getAspect(Eating).feed(this.prey.getAspect(Eating).fat);
+		// should this be in eating?
+		this.prey.getAspect(Eating).fat = 0;
+		return;
+	}
+		
+	var f = this.prey.position.minus(this.cell.position).normalize().scale(1.5);
+	this.cell.getAspect(Walking).applyForce(f);
+};
+
+Hunting.prototype.draw = function(ctx) {
+	if(this.prey) {
+		ctx.beginPath();
+		ctx.strokeStyle = '#FF0000';
+		ctx.moveTo(this.cell.position.x, this.cell.position.y);
+		ctx.lineTo(this.prey.position.x, this.prey.position.y);
+		ctx.stroke();
+	}
+};
+
+Hunting.prototype.priority = function() {
+	return this.prey === undefined ? 0 : this.cell.getAspect(Eating).hunger - this.prey.position.distance(this.cell.position)/100;
+}
+
+Hunting.prototype.report = function() {
+	return [this.priority()];
+}
+
+// ****************************************************************
+// behavior aspects
+
+function Fission() {
+}
+
+Fission.defaults = {
+	threshold: 14
+};
+
+Fission.prototype.perform = function() {
+	var eating = this.cell.getAspect(Eating);
+	if(eating.fat > this.threshold) {
+		this.cell.makeChild();
+	}
+}
+
+function Mating() {
+	this.horny = 0;
+}
+
+Mating.prototype.findMate = function() {
+	var tcell = this.cell;
+	var tab = _.filter(this.cell.getAspect(Looking).seen, function(c) {
+		return c.species === tcell.species && c.gender !== tcell.gender;
+	});
+	if(tab.length > 0)
+		return tab[0];
+}
+
+Mating.prototype.prepare = function() {
+	this.horny += Math.random()*0.01;
+	this.mate = this.findMate();
+}
+
+Mating.prototype.perform = function() {
+	if(this.mate === undefined)
+		return;
+	if(this.mate.position.distance(this.cell.position) < this.cell.getSize()+this.mate.getSize()+5) {
+		
+		this.cell.makeChild();
+		this.horny = -1;
+	}
+		
+	var f = this.mate.position.minus(this.cell.position).normalize();
+	this.cell.getAspect(Walking).applyForce(f);
+};
+
+Mating.prototype.draw = function(ctx) {
+	if(this.mate) {
+		ctx.beginPath();
+		ctx.strokeStyle = '#FF77DD';
+		ctx.moveTo(this.cell.position.x, this.cell.position.y);
+		ctx.lineTo(this.mate.position.x, this.mate.position.y);
+		ctx.stroke();
+	}
+};
+
+Mating.prototype.priority = function() {
+	return this.mate === undefined ? 0 : this.horny;
+}
+
+Mating.prototype.report = function() {
+	return [this.cell.gender, this.horny];
+}
+
+function Herding() {
+	this.vector = new Vector(0, 0);
+}
+
+Herding.defaults = {
+	strength: 1
+};
+
+Herding.prototype.prepare = function() {
+	var vel = new Vector(0, 0);
+	var loc = new Vector(0, 0);
+	this.vector = new Vector(0, 0);
+	
+	var tcell = this.cell;
+	var tab = _.filter(this.cell.getAspect(Looking).seen, function(c){
+		return c!==tcell && c.color===tcell.color && c.position.distance(tcell.position)<100;
+	});
+	var n = tab.length;
+	if(n>0) {
+		for(var i=0; i<n; i++) {
+			var c = tab[i];
+			vel = vel.plus(c.velocity);
+			loc = loc.plus(c.position);
+		}
+		var toLoc = loc.scale(1/n).minus(tcell.position).normalize();
+		var toVel = vel.scale(1/n).minus(tcell.getAspect(Walking).velocity).normalize();
+		
+		this.vector = toVel.plus(toLoc);
+	}
+};
+
+Herding.prototype.perform = function() {
+	this.cell.getAspect(Walking).applyForce(this.vector.normalize().scale(this.strength));
+};
+
 function Wandering() {
 	this.desired = new Vector(0, 0);
 	this.target = Vector.random(25, 25, width-50, height-50);
@@ -409,157 +527,49 @@ Wandering.prototype.priority = function() {
 	return 1;
 }
 
-function Eating() {
-	this.fat = 10;
-	this.hunger = 0;
+function RunningAway() {
 }
 
-Eating.prototype.perform = function() {	
-	this.fat -= (Math.random()*0.002)*this.fat;
-	
-	if(this.fat < 10)
-		this.hunger += (10-this.fat)/10;
-	if(this.fat > 15)
-		this.hunger = 0;
-}
-
-Eating.prototype.feed = function(amount) {
-	this.fat += amount;
-}
-
-Eating.prototype.report = function() {
-	return [this.fat, this.hunger];
-}
-
-function Looking() {
-	this.seen = [];
-}
-
-Looking.defaults = {
-	angle: Math.PI*2,
-	range: 200
+RunningAway.defaults = {
+	speedThreshold: 1,
+	angleThreshold: 0.5
 };
 
-function canvas_arrow(context, fromx, fromy, tox, toy){
-    var headlen = 10;   // length of head in pixels
-    var angle = Math.atan2(toy-fromy,tox-fromx);
-    context.moveTo(fromx, fromy);
-    context.lineTo(tox, toy);
-    context.lineTo(tox-headlen*Math.cos(angle-Math.PI/6),toy-headlen*Math.sin(angle-Math.PI/6));
-    context.moveTo(tox, toy);
-    context.lineTo(tox-headlen*Math.cos(angle+Math.PI/6),toy-headlen*Math.sin(angle+Math.PI/6));
+RunningAway.prototype.prepare = function() {
+	this.hunter = undefined;
+	var tcell = this.cell;
+	var tab = _.filter(this.cell.getAspect(Looking).seen, function(c){
+		return c.hasAspect(Hunting)
+			&& c.velocity.length() > this.speedThreshold
+			&& c.velocity.dotReduced(tcell.position.minus(c.position)) > this.angleThreshold;
+	}, this);
+	if(tab.length>0) {
+		this.hunter = tab[0];
+	}
 }
 
-Looking.prototype.draw = function(ctx) {
-	return;
-	ctx.beginPath();
-	ctx.strokeStyle = 'black';
-	/*
-	var m1 = [[Math.cos(this.angle),-Math.sin(this.angle)], [Math.sin(this.angle),Math.cos(this.angle)]];
-	var m2 = [[Math.cos(-this.angle),-Math.sin(-this.angle)], [Math.sin(-this.angle),Math.cos(-this.angle)]];
-	var v = this.cell.velocity.normalize().scale(this.range);
-	var v1 = v.mulMatrix(m1);
-	var p1 = this.cell.position.plus(v1);
-	var v2 = v.mulMatrix(m2);
-	var p2 = this.cell.position.plus(v2);
-	ctx.moveTo(this.cell.position.x, this.cell.position.y);
-	ctx.lineTo(p1.x, p1.y);
-	ctx.moveTo(this.cell.position.x, this.cell.position.y);
-	ctx.lineTo(p2.x, p2.y);
-	var a = v.angle();//Math.PI/4;
-	ctx.arc(this.cell.position.x, this.cell.position.y, this.range, a-this.angle, a+this.angle);
-	ctx.stroke();
-	*/
-	
-	for(var i=0; i<this.seen.length; i++) {
-		canvas_arrow(ctx, 
-			this.cell.position.x, this.cell.position.y, this.seen[i].position.x, this.seen[i].position.y
-		);
+RunningAway.prototype.perform = function() {
+	if(this.hunter === undefined)
+		return;
+	var f = this.cell.position.minus(this.hunter.position).normalize().scale(1.5);
+	this.cell.getAspect(Walking).applyForce(f);
+};
+
+RunningAway.prototype.draw = function(ctx) {
+	if(this.hunter) {
+		ctx.beginPath();
+		ctx.strokeStyle = 'yellow';
+		ctx.moveTo(this.cell.position.x, this.cell.position.y);
+		ctx.lineTo(this.hunter.position.x, this.hunter.position.y);
 		ctx.stroke();
 	}
-}
-
-Looking.prototype.perform = function() {
-	this.seen = this.cell.nearestCells(this.range);
-}
-
-Looking.prototype.report = function() {
-	return [this.seen.length];
-}
-
-function Photosynthesis() {
-}
-
-Photosynthesis.defaults = {
-	range: 200
 };
 
-Photosynthesis.prototype.perform = function() {
-	var eating = this.cell.getAspect(Eating);
-	var center = new Vector(width/2, height/2);
-	var d = this.range - (this.cell.position.distance(center));
-	if(d<0)
-		d = 0;
-	eating.feed(0.05*d/this.range);
-	this.cell.getAspect(Walking).applyForce(center.minus(this.cell.position).normalize().scale(0.1));
+RunningAway.prototype.priority = function() {
+	return this.hunter === undefined ? 0 : 100;
 }
 
-function Walking(args) {
-	this.velocity = new Vector(0, 0);
-	this.force = new Vector(0, 0);
-	this.forceCount = 0;
+RunningAway.prototype.report = function() {
+	return this.hunter === undefined ? [] : [1];
 }
 
-Walking.defaults = {
-	topSpeed: 2,
-	agility: 2
-};
-
-Walking.prototype.applyForce = function(f) {
-	this.force = this.force.plus(f);
-	this.forceCount++;
-}
-
-Walking.prototype.prepare = function() {
-	this.force = new Vector(0, 0);
-	this.forceCount = 0;
-}
-
-Walking.prototype.perform = function() {
-
-	if(this.forceCount > 0) {
-		this.force = this.force.scale(1/this.forceCount).scale(0.6*(20-this.cell.getSize())/20 + 0.7);
-		this.velocity = this.velocity.plus(this.force.scale(this.agility)).capLength(this.topSpeed);
-	}
-	
-	// for legacy
-	this.cell.velocity = this.velocity = this.velocity.scale(0.9);
-	this.cell.position = this.cell.position.plus(this.velocity);
-}
-
-Walking.prototype.draw = function(ctx) {
-	ctx.beginPath();
-	ctx.strokeStyle = 'lime';
-	ctx.moveTo(this.cell.position.x, this.cell.position.y);
-	var t = this.cell.position.plus(this.force.scale(50));
-	ctx.lineTo(t.x, t.y);
-	ctx.stroke();
-}
-
-Walking.prototype.report = function() {
-	return [this.forceCount];
-}
-
-function Fission() {
-}
-
-Fission.defaults = {
-	threshold: 14
-};
-
-Fission.prototype.perform = function() {
-	var eating = this.cell.getAspect(Eating);
-	if(eating.fat > this.threshold) {
-		this.cell.makeChild();
-	}
-}
